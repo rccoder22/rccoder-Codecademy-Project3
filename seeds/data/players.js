6e887699a2e43394d8e75cd6ea671808e8305ca2
Richard Coney <richardconey46@gmail.com>
2026-03-23 20:34:28 -0400

    Changed to bats and throws for Left and Right

--- seeds/data/players.js
+++ seeds/data/players.js
@@ -1,17 +1,10 @@
-"Bryce Harper"	"Philadelphia Phillies"	"First Base"	"Left"	"Right"	"1992-10-16"	"harpb003"
-"Ryan Howard"	"Philadelphia Phillies"	"First Base"	"Left"	"Left"	"1979-11-19"	"howar001"
-"Derek Jeter"	"New York Yankees"	"Shortstop"	"Right"	"Right"	"1974-06-26"	"jeted001"
-"Aaron Judge"	"New York Yankees"	"Right Field"	"Right"	"Right"	"1992-04-26"	"judga001"
-"Frank Thomas"	"Chicago White Sox"	"First Base"	"Right"	"Right"	"1968-05-27"	"thomf001"
-"Kyle Schwarber"	"Philadelphia Phillies"	"Designated Hitter"	"Left"	"Right"	"1993-03-05"	"schwk001"
-
 module.exports = [
     {
         name: "Bryce Harper",
         team: "Philadelphia Phillies",
         position: "First Base",
-        handedness: "Left",
-        battingHand: "Right",
+        bats: "Left",
+        throws: "Right",
         birthDate: "1992-10-16",
         playerId: "harpb003"
     },
@@ -19,8 +12,8 @@ module.exports = [
         name: "Ryan Howard",
         team: "Philadelphia Phillies",
         position: "First Base",
-        handedness: "Left",
-        battingHand: "Left",
+        bats: "Left",
+        throws: "Left",
         birthDate: "1979-11-19",
         playerId: "howar001"
     },
@@ -28,8 +21,8 @@ module.exports = [
         name: "Derek Jeter",
         team: "New York Yankees",
         position: "Shortstop",
-        handedness: "Right",
-        battingHand: "Right",
+        bats: "Right",
+        throws: "Right",
         birthDate: "1974-06-26",
         playerId: "jeted001"
     },
@@ -37,8 +30,8 @@ module.exports = [
         name: "Aaron Judge",
         team: "New York Yankees",
         position: "Right Field",
-        handedness: "Right",
-        battingHand: "Right",
+        bats: "Right",
+        throws: "Right",
         birthDate: "1992-04-26",
         playerId: "judga001"
     },
@@ -46,8 +39,8 @@ module.exports = [
         name: "Frank Thomas",
         team: "Chicago White Sox",
         position: "First Base",
-        handedness: "Right",
-        battingHand: "Right",
+        bats: "Right",
+        throws: "Right",
         birthDate: "1968-05-27",
         playerId: "thomf001"
     },
@@ -55,8 +48,8 @@ module.exports = [
         name: "Kyle Schwarber",
         team: "Philadelphia Phillies",
         position: "Designated Hitter",
-        handedness: "Left",
-        battingHand: "Right",
+        bats: "Left",
+        throws: "Right",
         birthDate: "1993-03-05",
         playerId: "schwk001"
     }
